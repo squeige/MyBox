0d852590b80d87c1ebdfb60223ddabc4e711a6d3
luigi <virtual.employee@gmail.com>
2025-03-05 20:51:01 +0000

    initial commit

---
+++ executable_app_install.sh
@@ -0,0 +1,14 @@
+# Update apt and install basic apps
+sudo apt update
+sudo apt dist-upgrade
+sudo apt install -y curl wget tar unzip openssh-server git
+
+# Install Chezmoish 
+sh -c "$(curl -fsLS get.chezmoi.io)"
+
+# Install Neovim
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
